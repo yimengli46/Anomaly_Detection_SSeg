1c5cd44eb2d3bbeecfa7013b39fd5d8596652324
yimengli46 <yli44@ARGO-1.orc.gmu.edu>
2020-11-24 16:15:08 -0500

    add more datasets to extract_feature

--- extract_features.py
+++ extract_features.py
@@ -5,15 +5,15 @@ from parameters import Parameters
 import torch
 import torch.nn.functional as F
 from PIL import Image
-from dataloaders.datasets import LostAndFound, RoadAnomaly, cityscapes, cityscapes_fewer_classes
+from dataloaders.datasets import LostAndFound, RoadAnomaly, cityscapes, cityscapes_fewer_classes, Fishyscapes
 from torch.utils.data import DataLoader
 import cv2
 
 mode = 'resNet' #'resNet', 'mobileNet'
-saved_folder = 'results_deeplab/{}'.format('resNet_features') 
+saved_folder = 'results_deeplab/{}/Fishyscapes'.format('resNet_features') 
 num_class = 8
 
-dataset = 'LostAndFound' #LostAndFound, RoadAnomaly, cityscapes
+dataset = 'Fishyscapes' #LostAndFound, RoadAnomaly, cityscapes, Fishyscapes
 
 if dataset == 'LostAndFound':
     dataset_folder = '/projects/kosecka/yimeng/Datasets/Lost_and_Found'
@@ -21,6 +21,8 @@ elif dataset == 'RoadAnomaly':
     dataset_folder = '/projects/kosecka/yimeng/Datasets/RoadAnomaly'
 elif dataset == 'cityscapes':
     dataset_folder = 'data/cityscapes'
+elif dataset == 'Fishyscapes':
+    dataset_folder = '/projects/kosecka/yimeng/Datasets/Fishyscapes_Static'
 
 #====================================================== change the parameters============================================================
 par = Parameters()
@@ -45,10 +47,13 @@ elif dataset == 'RoadAnomaly':
 elif dataset == 'cityscapes':
     dataset_val = cityscapes_fewer_classes.CityscapesDataset_fewer(par, dataset_dir=dataset_folder, split='val')
     dataloader_val = DataLoader(dataset_val, batch_size=par.test_batch_size, shuffle=False, num_workers=int(par.test_batch_size/2))
+elif dataset == 'Fishyscapes':
+    dataset_val = Fishyscapes.Fishyscapes(par, dataset_dir=dataset_folder)
+    dataloader_val = DataLoader(dataset_val, batch_size=par.test_batch_size, shuffle=False, num_workers=int(par.test_batch_size/2))
 
 #big_outlier_list = [2, 3, 4, 7, 10, 11, 15, 16, 25, 27, 31, 33, 34, 35, 38, 40, 45, 46, 48, 50, 51, 54, 57, 60, 61, 63, 65, 
 #    68, 71, 72, 74, 76, 83, 84, 85, 86, 91, 93, 95]
-big_outlier_list = [2, 4, 10, 16, 27, 38, 45, 50, 51, 61, 65, 68, 74, 76, 83, 84, 95]
+#big_outlier_list = [2, 4, 10, 16, 27, 38, 45, 50, 51, 61, 65, 68, 74, 76, 83, 84, 95]
 
 #================================================================================================================================
 # Define network
@@ -67,8 +72,8 @@ if par.resume is not None:
     model.eval()
     count = 0
     for iter_num, sample in enumerate(dataloader_val):
-        if dataset == 'cityscapes' and iter_num == 10:
-            break
+        #if dataset == 'cityscapes' and iter_num == 10:
+        #    break
         print('iter_num = {}'.format(iter_num))
         images, targets = sample['image'], sample['label']
         #print('images = {}'.format(images.shape))
@@ -77,32 +82,35 @@ if par.resume is not None:
 
         #================================================ compute loss =============================================
         with torch.no_grad():
-            output, z = model(images) #output.shape = batch_size x num_classes x H x W
+            output, z, _ = model(images) #output.shape = batch_size x num_classes x H x W
             print('z.shape = {}'.format(z.shape))
-            H, W = targets.shape[-2:]
-            input_shape = (int(H/2), int(W/2))
-            z_interpolated = F.interpolate(z, size=input_shape, mode='bilinear', align_corners=False)
-            print('interpolated z.shape = {}'.format(z_interpolated.shape))
+            #H, W = targets.shape[-2:]
+            #input_shape = (int(H/2), int(W/2))
+            #z_interpolated = F.interpolate(z, size=input_shape, mode='bilinear', align_corners=False)
+            #print('interpolated z.shape = {}'.format(z_interpolated.shape))
             
-            z_interpolated = z_interpolated.data.cpu().numpy()
-            targets = targets.numpy().astype(np.uint8)
+            #z_interpolated = z_interpolated.data.cpu().numpy()
+            z = z.data.cpu().numpy()
+            #targets = targets.numpy().astype(np.uint8)
             #assert 1==2
 
         N, _, _, _ = z.shape
         for i in range(N):
-            result = {}
-            result['feature'] = z_interpolated[i]
-            target = cv2.resize(targets[i], input_shape[::-1], cv2.INTER_NEAREST)
-            print('target.shape = {}'.format(target.shape))
-            result['label'] = target
+            #result = {}
+            #result['feature'] = z_interpolated[i]
+            #target = cv2.resize(targets[i], input_shape[::-1], cv2.INTER_NEAREST)
+            #print('target.shape = {}'.format(target.shape))
+            #result['label'] = target
+            result = z[i]
+            #assert 1==2
 
-            if dataset == 'LostAndFound' and count in big_outlier_list:
-                np.save('{}/{}_{}.npy'.format(saved_folder, count, dataset), result)
-            if dataset == 'cityscapes':
-                np.save('{}/{}_{}.npy'.format(saved_folder, count, dataset), result)
+            
+            np.save('{}/{}_deeplab_ft.npy'.format(saved_folder, count), result)
+
 
             #assert 1==2
             count += 1
+        #assert 1==2
 
 
 
